762e9f2aaf9cd25125925cbc364caeb2ac11ce50
Vedik Saini <vediksaini0704@gmail.com>
2025-03-19 10:28:03 +0530

    direction enums and apply pattern matching

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+enum Direction {
+    North,
+    South,
+    East,
+    West
+}
+
+fn main(){
+    let direction = Direction::North;
+
+    steer(direction);
+}
+
+
+fn steer(dir:Direction){
+    match dir {
+        Direction::East => println!("East direction"),
+        Direction::West => println!("West direction"),
+        // Direction::North => println!("North direction"),
+        // Direction::South => println!("South direction"),
+
+        // ? Catch of other cases
+        _ => println!("Horizontal direction"),
+
+    }
 }
