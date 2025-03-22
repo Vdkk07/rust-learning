b100e1ca74c28118ea83761e8ce46c0ebbb85cb3
Vedik Saini <vediksaini0704@gmail.com>
2025-03-22 07:54:50 +0530

    Explore Rust iterators: consuming adapters, iterator adapters (map, filter) and collect method

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -46,4 +46,42 @@ fn main (){
     // ! v4 cannot be used because it is no longer the owner
     // println!("v4: {:?}", v4); 
 
+    // # Consuming adapters -> method define on the iterator trait that consume the iterator
+    let v5 = vec![1,2,3,4,5];
+    let v5_iter = v5.iter();
+
+    let sum:i32 = v5_iter.sum();
+
+    println!("sum is: {}", sum);
+
+    // ! We cannot use the iterator here because its value get consumed 
+    // println!("v5_iter: {:?}", v5_iter);
+
+    // # Iterator adapters -> produce different iterator by changing some aspect of the original iterator
+    // ? Map()
+    let v6 = vec![1,2,3,4,5];
+    let v6_iter = v6.iter();
+    let v6_iter_2 = v6_iter.map(|x| x + 1);
+
+    for val in v6_iter_2 {
+        println!("v6_iter_2 is {}", val);
+    };
+
+    // ? Filter()
+    let v7 = vec![1,2,3,4,5];
+    let v7_iter = v7.iter();
+    let v7_iter_3 = v7_iter.filter(|x| *x % 2 == 0);
+
+    for val in v7_iter_3 {
+        println!("even values in v6_iter_3 is {}", val);
+    }
+
+    // # using collect() method on iterator for converting back to vector
+    let v8 = vec![1,2,3,4,5];
+    let v8_iter = v8.iter().filter(|x| *x % 2 == 1 ).map(|x| x * 2);
+
+    let v8_vec: Vec<i32 >= v8_iter.collect();
+
+    println!("new v8 vector: {:?}", v8_vec);
+
 }
