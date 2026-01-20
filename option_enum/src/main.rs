d589c3d296a899373f7f4f77b48028eec5b9ec02
Vedik Saini <vediksaini0704@gmail.com>
2026-01-20 19:25:22 +0530

    Practice option enum

--- option_enum/src/main.rs
+++ option_enum/src/main.rs
@@ -6,6 +6,23 @@ fn main(){
        None => println!("Value not found"),
        Some(val) => println!("i found at index {}", val),
    }
+
+   // ! Practicing
+   let res = division(5.89, 3.98);
+
+   match res {
+       None => println!("Cannot divided by 0"),
+       Some(x)=> println!("{}", x)
+   }
+}
+
+fn division(x: f64, y: f64) -> Option<f64> {
+   if y == 0.0{
+      None
+   }
+   else {
+       Some(x/y)
+   }
 }
 
 fn find_first_i(str:String)-> Option<u32>{
