942217e18cccbb02354f933961e36e8f01dc81ae
Vedik Saini <vediksaini0704@gmail.com>
2026-01-24 10:44:47 +0530

    practice traits, operator overloading and drop

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 trait Summary{
     //? default implementation
     fn summarize(&self)-> String{
@@ -20,6 +22,68 @@ impl Summary for User{
         return format!("The name is {} and the age is {}", self.name , self.age);
     }
 }
+// ! New Learning
+trait Shape {
+    fn area(&self)-> u32;
+}
+
+struct Rectangle{
+    width: u32,
+    height: u32
+}
+
+struct Circle{
+    radius : f64
+}
+
+impl Shape for Rectangle {
+    fn area(&self)-> u32 {
+        self.height * self.width
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self)-> u32 {
+        (3.14 * self.radius * self.radius) as u32
+    }
+}
+
+// ! Overload traits
+
+// ? Add trait
+struct A;
+struct B;
+#[derive(Debug)]
+struct AB;
+#[derive(Debug)]
+struct BA;
+
+impl ops::Add<B> for A {
+    type Output = AB;
+
+    fn add(self, rhs: B) -> AB {
+        AB
+    }
+}
+
+impl ops::Add <A> for B{
+    type Output = BA;
+
+    fn add(self, rhs: A) -> BA {
+        BA
+    }
+}
+
+// ? Drop Trait
+struct C {
+    c: String
+}
+
+impl Drop for C {
+    fn drop(&mut self) {
+        println!("dropped {}", self.c)
+    }
+}
 
 fn main(){
     let user = User{
@@ -30,6 +94,39 @@ fn main(){
 
     let f = Fix{};
     notify(f);
+
+    // ! New Learning
+    let r = Rectangle{
+        width: 747,
+        height: 987
+    };
+    let c = Circle {
+        radius: 89.3
+    };
+
+    println!("Rect: {}, Circle: {}", r.area(), c.area());
+
+    // ! Overload trait
+    // ? Add trait
+    println!("{:?}", A + B);
+    println!("{:?}", B + A);
+
+    // ? Drop trait
+    let a =  C{c: String::from("a")};
+    {
+        let b =  C{c: String::from("b")};
+
+        {
+            let c =  C{c: String::from("c")};
+
+            println!("leaving inner scope 2")
+        }
+            println!("leaving inner scope 1")
+    }
+
+    drop(a);
+    println!("program ending")
+
 } 
 
 // # Traits as parameters
